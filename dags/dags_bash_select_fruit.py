b8eb7dea380ae8c6681817e15379d943578d0d28
JBCHA <pololi8902@gmail.com>
2025-03-18 14:00:40 +0900

    select_fruit 추가

--- dags/dags_bash_select_fruit.py
+++ dags/dags_bash_select_fruit.py
@@ -12,13 +12,13 @@ with DAG(
 ) as dag:
     
     t1_orange = BashOperator(
-        task_id="t1_orange"
-        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE"
+        task_id="t1_orange",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh ORANGE",
     )
     
     t2_avocado = BashOperator(
-        task_id="t1_orange"
-        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO"
+        task_id="t1_orange",
+        bash_command="/opt/airflow/plugins/shell/select_fruit.sh AVOCADO",
     )
     
     t1_orange >> t2_avocado
