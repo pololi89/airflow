d399429f61ff0c30555a97576a92666ec07ccff3
JBCHA <pololi8902@gmail.com>
2025-03-24 16:04:45 +0900

    '수정'

--- dags/dags_python_template.py
+++ dags/dags_python_template.py
@@ -5,7 +5,7 @@ from airflow.operators.python import PythonOperator
 from airflow.decorators import task
 
 with DAG(
-    dag_id="dags_python_show_templates",
+    dag_id="dags_python_template",
     schedule="30 9 * * *",
     start_date=pendulum.datetime(2025, 3, 15, tz="Asia/Seoul"),
     catchup=False
@@ -16,5 +16,18 @@ with DAG(
         print(end_date)
         
     python_t1 = PythonOperator(
-        
+        task_id='python_t1',
+        python_collable=python_function1,
+        op_kwargs={'start_date':'{{data_interval_start | ds}}', 'end_date':{{data_interval_end | ds}}}
     )
+    
+    @task(task_id='python_t2')
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds:' + kwargs['ds'])
+        print('ts:' + kwargs['ts'])
+        print('data_interval_start:' + str(kwargs['data_interval_start']))
+        print('data_interval_end:' + str(kwargs['data_interval_end']))
+        print('task_instance:' + str(kwargs['ti']))
+        
+    python_t1 >> python_function2()
