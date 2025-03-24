23b2dfbf866cc87f69ca8e7057aa436472918e71
JBCHA <pololi8902@gmail.com>
2025-03-24 15:59:36 +0900

    python template

---
+++ dags/dags_python_template.py
@@ -0,0 +1,20 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 3, 15, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+    
+    def python_function1(start_date, end_date, **kwargs):
+        print(start_date)
+        print(end_date)
+        
+    python_t1 = PythonOperator(
+        
+    )
