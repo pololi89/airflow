c666df657a4c51247439382a8af1917142364c53
JBCHA <pololi8902@gmail.com>
2025-03-24 15:39:56 +0900

    show templates

---
+++ dags/dags_python_show_templates.py
@@ -0,0 +1,17 @@
+import pendulum
+from airflow.models.dag import DAG
+from airflow.decorators import task
+
+with DAG(
+    dag_id="dags_python_show_templates",
+    schedule="30 9 * * *",
+    start_date=pendulum.datetime(2025, 3, 15, tz="Asia/Seoul"),
+    catchup=True,
+) as dag:
+    
+    @task(task_id='python_task')
+    def show_templates(**kwargs):
+        from pprint import pprint
+        pprint(kwargs)
+        
+    show_templates()
