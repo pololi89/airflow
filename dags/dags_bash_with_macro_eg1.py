08b27f453161f450c5f2761807935ceba15e6ae4
JBCHA <pololi8902@gmail.com>
2025-03-25 09:01:01 +0900

    macro egs

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -4,7 +4,7 @@ import pendulum
 from airflow.operators.bash import BashOperator
 
 with DAG(
-    dag_id="dags_bash_operator_with_template",
+    dag_id="dags_bash_with_macro_eg1",
     schedule="10 0 L * *",
     start_date=pendulum.datetime(2025, 1, 1, tz="Asia/Seoul"),
     catchup=False,
@@ -12,8 +12,8 @@ with DAG(
     # START_DATE: 전월 말일, END_DATE: 1일 전
     bash_task_1 = BashOperator(
         task_id='bash_task_1',
-        env={'START_DATE':'{{ }}',
-             'END_DATE':'{{}}'
+        env={'START_DATE':'{{ data_interval_start.in_timezone("Asia/Seoul") | ds }}',
+             'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=-1)) | ds }}'
         },
         bash_command='echo "START_DATE: $START_DATE" && echo "END_DATE: $END_DATE"'
     )
