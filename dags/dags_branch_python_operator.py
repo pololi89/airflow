321445fa4c73a89dd33f17aebe4fbbd415643664
JBCHA <pololi8902@gmail.com>
2025-03-25 16:54:48 +0900

    '수정'

--- dags/dags_branch_python_operator.py
+++ dags/dags_branch_python_operator.py
@@ -12,38 +12,38 @@ with DAG(
 ) as dag:
     def select_random():
         import random
-        
+    
         item_lst = ['A','B','C']
         selected_item = random.choice(item_lst)
         if selected_item == 'A':
             retrun 'task_a'
         elif selected_item in ['B','C']:
             return ['task_b','task_c']
+    
+    python_branch_task = BranchPythonOperator(
+        task_id='python_branch_task',
+        python_callable=select_random
+    )
+    
+    def common_func(**kwargs):
+        print(kwargs['selected'])
         
-        python_branch_task = BranchPythonOperator(
-            task_id='python_branch_task',
-            python_callable=select_random
-        )
-        
-        def common_func(**kwargs):
-            print(kwargs['selected'])
-            
-        task_a = PythonOperator(
-            task_id='task_a',
-            python_callable=common_func,
-            op_kwargs={'selected':'A'}
-        )
-        
-        task_b = PythonOperator(
-            task_id='task_b',
-            python_callable=common_func,
-            op_kwargs={'selected':'B'}
-        )
-        
-        task_c = PythonOperator(
-            task_id='task_c',
-            python_callable=common_func,
-            op_kwargs={'selected':'C'}
-        )
-        
-        python_branch_task >> [task_a, task_b, task_c]
+    task_a = PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected':'A'}
+    )
+    
+    task_b = PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected':'B'}
+    )
+    
+    task_c = PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected':'C'}
+    )
+    
+    python_branch_task >> [task_a, task_b, task_c]
