ac9bf1fd38fe240e747518a643e25610db886c7e
JBCHA <pololi8902@gmail.com>
2025-04-08 16:41:06 +0900

    modify

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -47,7 +47,7 @@ class SeoulApiToCsvOperator(BaseOperator):
                    }
 
         request_url = f'{base_url}/{start_row}/{end_row}/'
-        if self.bsse_dt is not None:
+        if self.base_dt is not None:
             request_url = f'{base_url}/{start_row}/{end_row}/{self.base_dt}'
         response = requests.get(request_url, headers)    
         contents = json.loads(response.text)
