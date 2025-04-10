5eeeff89354b39a47ddfca606c48cf12f5e68434
JBCHA <pololi8902@gmail.com>
2025-04-10 16:35:59 +0900

    modify

--- dags/dags_file_sensor.py
+++ dags/dags_file_sensor.py
@@ -6,7 +6,7 @@ with DAG(
     dag_id='dags_file_sensor',
     start_date=pendulum.datetime(2025,4,1,tz='Asia/Seoul'),
     schedule='0 7 * * *',
-    chatchup=False
+    catchup=False
 ) as dag:
     tvCorona19VaccineStatNew_sensor = FileSensor(
         task_id='tvCorona19VaccineStatNew_sensor',
