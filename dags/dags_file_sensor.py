668b805bcc2f104fbe64ab785c666b3d2f17e5e1
JBCHA <pololi8902@gmail.com>
2025-04-10 16:32:46 +0900

    modify

--- dags/dags_file_sensor.py
+++ dags/dags_file_sensor.py
@@ -1,4 +1,4 @@
-from airflow import dag
+from airflow import DAG
 from airflow.sensors.filesystem import FileSensor
 import pendulum
 
@@ -6,12 +6,12 @@ with DAG(
     dag_id='dags_file_sensor',
     start_date=pendulum.datetime(2025,4,1,tz='Asia/Seoul'),
     schedule='0 7 * * *',
-    chtchup=False
+    chatchup=False
 ) as dag:
     tvCorona19VaccineStatNew_sensor = FileSensor(
         task_id='tvCorona19VaccineStatNew_sensor',
         fs_conn_id='conn_file_opt_airflow_files',
-        filepath='tvCorona19VaccinestatNew/{{data_interval_end.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv',
+        filepath='tvCorona19VaccinestatNew/{{execution_date.in_timezone("Asia/Seoul") | ds_nodash}}/tvCorona19VaccinestatNew.csv',
         recursive=False,
         poke_interval=60,
         timeout=60*60*24,
